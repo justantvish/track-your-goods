6676a846004f69ce70c43dab559e544493ee6f0c
justantvish <justantvish@gmail.com>
2024-05-26 18:50:12 +0300

    minor changes

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,15 @@
+// export const COLORS = {
+//     bg: '#D9EDBF',
+//     main: '#ff8800',
+//     text: '#2C7865',
+//     add: '#90D26D',
+//     bgAlt: '#f5ffe8',
+// }
+
 export const COLORS = {
-    bg: '#D9EDBF',
-    main: '#FF9800',
-    text: '#2C7865',
-    add: '#90D26D',
-    bgAlt: '#f5ffe8',
+    bg: '#E6FF94',
+    main: '#ff8800',
+    text: '#006769',
+    add: '#9DDE8B',
+    bgAlt: '#D9EDBF',
 }
